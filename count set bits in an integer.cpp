d6e17bff8a6042d12fa06a8ef66e8ebc55e4ae8d
Atharva Deshmukh <81254891+Atharva-Deshmukh@users.noreply.github.com>
2021-06-17 17:00:11 +0530

    Add files via upload

---
+++ count set bits in an integer.cpp
@@ -0,0 +1,35 @@
+//baari baari (starting from rightmost bit),bitwise AND karte jao the last bit with 1 (0001). Agar 1 hai rightmost bit 
+//to (n&1)=1 aayga and count increases, else count me 0 add hota jaayga
+
+//Issue:
+//TC O(logn) kaise aaya ye nhi pata. kuch to log2n ka logic hai. Figure it out later
+
+#include<iostream>
+using namespace std;
+
+unsigned int countSetBits(unsigned int n)
+{
+	unsigned int count=0;
+	
+	//while(n) is equivalent to while(n!=0)
+	while(n)
+	{
+		count=count+(n&1);
+		n=n>>1;    //rightshift by 1
+	}
+	return count;
+}
+
+int main() 
+{ 
+	//bitwise operations becomes undefined in negative numbers. so unsigned int is used.
+	unsigned int n;
+	
+	cout<<"\nEnter the number ";
+	cin>>n;
+	
+	//calling our function
+	cout<<"\nThe number of set bits in "<<n<<" is "<<" "<<countSetBits(n);
+	
+	return 0;
+}
